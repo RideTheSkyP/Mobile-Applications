329da518ee3f5a1da4087df1a1ffe5c6612242f0
Push <49253201+RideTheSkyP@users.noreply.github.com>
2021-03-29 23:57:14 +0200

    Added clicks handler. Add new activity.

--- List3/todo/src/main/java/com/lab3/todo/MainActivity.kt
+++ List3/todo/src/main/java/com/lab3/todo/MainActivity.kt
@@ -1,42 +1,76 @@
 package com.lab3.todo
 
-import android.content.ClipData.Item
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.lab3.todo.MyRecyclerViewAdapter.ItemClickListener
 import com.lab3.todo.databinding.ActivityMainBinding
+import com.lab3.todo.databinding.InfoPickerBinding
 
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(), ItemClickListener
 {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingInfo: InfoPickerBinding
+    var adapter: MyRecyclerViewAdapter? = null
+    val recycleList: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        bindingInfo = InfoPickerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val itemList = ArrayList<Item>()
-//        var recyclerAdapter = binding.recyclerView.layoutManager
-//        val itemArrayAdapter = ItemArrayAdapter(itemList)
-//        recyclerAdapter = LinearLayoutManager(this)
-//        recyclerAdapter.addView(TextView)
-//        binding.recyclerView.itemAnimator = DefaultItemAnimator()
-//        binding.recyclerView.adapter = itemList
+        recycleList.add("Item0")
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MyRecyclerViewAdapter(this, recycleList)
+        adapter!!.setClickListener(this)
+        adapter!!.setOnLongClickListener(this)
+        recyclerView.adapter = adapter
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+    }
+
+    override fun onItemClick(view: View?, position: Int)
+    {
+        Toast.makeText(
+                this,
+                "Click " + adapter!!.getItem(position) + " on row number " + position,
+                Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onLongClick(view: View?, position: Int)
+    {
+        Toast.makeText(
+                this,
+                "Long click " + adapter!!.getItem(position) + " on row number " + position,
+                Toast.LENGTH_SHORT
+        ).show()
     }
 
     fun onTaskAddButton(view: View)
     {
-        val valueTV = TextView(this)
-        valueTV.text = "hallo hallo"
-        valueTV.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-        binding.recyclerView.layoutManager.addView(valueTV)
+        setContentView(bindingInfo.root)
+//        val length = adapter?.itemCount ?: 0
+//        recycleList.add(length, "Item")
+//        adapter?.notifyItemInserted(length)
+//        recycleList.add("Item")
+//        adapter?.notifyDataSetChanged()
     }
 }
+
+//@Entity(tableName="events")
+//data class Events
+//{
+//    private
+//}
